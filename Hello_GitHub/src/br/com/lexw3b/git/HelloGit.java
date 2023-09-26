0b785037d46a4589eb891b1146d0ca4527562e9e
ALEXANDRE EVANGELISTA <78969878+LEXW3B@users.noreply.github.com>
2023-09-26 07:29:23 -0300

    Add files via upload

---
+++ Hello_GitHub/src/br/com/lexw3b/git/HelloGit.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package br.com.lexw3b.git;
+
+/**
+ * @author alexandre
+ * Integração da IDE Eclipse com o GitHub
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+
+}
