63bcf0750c5633bda410f5974fea84c59edef77d
ALEXANDRE EVANGELISTA <78969878+LEXW3B@users.noreply.github.com>
2023-09-26 08:31:32 -0300

    raiz quadrada usando a class Math e chamando o método sqrt(java.lang.Math).

---
+++ RaizQuadrada/src/raiz/quadrada/RaizQuadradaDeVinteECinco.java
@@ -0,0 +1,10 @@
+package raiz.quadrada;
+import java.lang.Math;
+
+public class RaizQuadradaDeVinteECinco {
+
+	public static void main(String[] args) {
+		double resultado = Math.sqrt(25);
+		System.out.print(resultado);
+	}
+}
